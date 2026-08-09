3e4b5ba97c4ce83137a4eb03f996a48c8329a61d
luizidebook <47362223+luizidebook@users.noreply.github.com>
2026-08-08 23:04:41 -0300

    fix: align stdio bootstrap with sdk return type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { serveStdio } from '@modelcontextprotocol/server/stdio';
 import { createTocaServer } from './server.js';
 
-await serveStdio(() => createTocaServer());
+serveStdio(() => createTocaServer());
