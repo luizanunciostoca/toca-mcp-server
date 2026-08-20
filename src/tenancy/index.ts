54bc226532cd8306f274b805d89312e46bde14fa
Idebook Digital <luizanunciostoca@gmail.com>
2026-08-20 02:19:03 -0300

    feat(tenancy): expose tenant foundation primitives

---
+++ src/tenancy/index.ts
@@ -0,0 +1,6 @@
+export * from './contracts.js';
+export * from './tenant-boundary.js';
+export * from './tenant-configuration.js';
+export * from './tenant-credential-resolver.js';
+export * from './tenant-policy.js';
+export * from './tenant-registry-resolver.js';
