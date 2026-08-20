b67d1a694271d0243e998847bd9f2336a56f3c1a
Idebook Digital <luizanunciostoca@gmail.com>
2026-08-20 02:00:02 -0300

    feat(assets): expose marketing autopilot asset supply contract

---
+++ src/contracts/marketing-autopilot-assets.ts
@@ -0,0 +1,8 @@
+export {
+  assetIntelligenceQueryModeSchema as marketingAutopilotAssetSupplyModeSchema,
+  assetIntelligenceQuerySchema as marketingAutopilotAssetSupplyRequestSchema,
+  marketingAutopilotAssetCandidateSchema,
+  marketingAutopilotAssetQueryResultSchema as marketingAutopilotAssetSupplyResultSchema,
+  type AssetIntelligenceQuery as MarketingAutopilotAssetSupplyRequest,
+  type MarketingAutopilotAssetQueryResult as MarketingAutopilotAssetSupplyResult,
+} from '../assets/asset-intelligence-query.js';
